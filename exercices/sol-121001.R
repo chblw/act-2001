2886cadd7a648f70e125dd62b4506b84720fa53a
alec42 <alecvanrassel@icloud.com>
2020-09-18 10:16:19 -0400

    Update sol-121001.R
    Proposition d'une deuxième approche y compris un code pour générer un joli graphique en R des simulations.

--- exercices/sol-121001.R
+++ exercices/sol-121001.R
@@ -1,4 +1,4 @@
-
+####  Première approche ####
 # a) ----------------------------------------------------------------------
 
 set.seed(2018)
@@ -40,3 +40,125 @@ for(i in seq_along(Z)) {
 }
 
 sort(Z)[0.99 * m]
+
+####  Deuxième approche ####
+# On définit une fonction générale qui permet de simuler des réalisations d'une police d'asssurance avec n'importe quelle distribution
+#   sous-jacente.
+simulateLifeClaims <- function(delta, t, lambda, distributionClaims, distributionParameters, seed, numberRepetitions, numberEventsPP1) {
+    require(rlang)  ## for the exec function which allows us to call a function uing text
+    
+    interEventTime <- vector(mode = "list", length = numberRepetitions)
+    eventTime <- vector(mode = "list", length = numberRepetitions)
+    eventTimeFiltered <- vector(mode = "list", length = numberRepetitions)
+    claimCostsIndividuals <- vector(mode = "list", length = numberRepetitions)
+    claimCostsAggregated <- numeric(numberRepetitions)
+    numberEvents <- numeric(numberRepetitions)
+    
+    set.seed(seed)
+    for (i in 1:numberRepetitions) {
+        interEventTime[[i]] <- rexp(numberEventsPP1, lambda)
+        eventTime[[i]] <- cumsum(interEventTime[[i]])
+        ##  We need to ensure that enough events are simulated and 
+        ##      so check the last event occured after t.
+        stopifnot(eventTime[[i]][numberEventsPP1] >= t)
+        
+        ##  We only keep the simulated times relevent to the interval
+        eventTimeFiltered[[i]] <- eventTime[[i]][eventTime[[i]] <= t]
+        numberEvents[[i]] <- length(eventTimeFiltered[[i]])
+        
+        ##  We simulate individual claim costs (X_k) for the given distribution 
+        ##      which are then given.
+        ##
+        ##  exec function: for example if distributionClaims = "lnorm" then 
+        ##      this call is equivalent to rlnorm(numberEvents[[i]], distributionParameters[1], distributionParameters[2])
+        claimCostsIndividuals[[i]] <- rlang::exec(
+            .fn = paste0("r", distributionClaims),
+            numberEvents[[i]],
+            !!!distributionParameters
+        )
+        claimCostsAggregated[i] <- sum(exp(-delta * eventTimeFiltered[[i]]) * claimCostsIndividuals[[i]])
+    }
+    list(
+        "Aggregate Costs" = claimCostsAggregated,
+        "Time of Events" = eventTimeFiltered,
+        "Individual Cost of Events" = claimCostsIndividuals,
+        "Number of Events" = numberEvents
+    )
+}
+# Pour le numéro 2 : 
+simulatedClaimsData <- simulateLifeClaims(
+    delta = 0.03, t = 10, lambda = 1,
+    distributionClaims = "lnorm", distributionParameters = list(log(10) - 0.5, 1), 
+    seed = 2018, numberRepetitions = 1E4, numberEventsPP1 = 1E2
+)
+
+# Visualisation des montants aggrégés simulés :
+library(ggplot2)
+ggplot(data = data.frame(cost = simulatedClaimsData$`Aggregate Costs`, simulation = seq_len(1E4)), aes(cost)) + 
+    geom_histogram(fill = "gray83", bins = 60) + 
+    theme_classic() + 
+    labs(
+        y = "Fréquence",
+        x = "Coût"
+    )
+
+## graphique plus intense qui permet de mieux visualiser :
+simulatedAggregateCosts <- simulatedClaimsData$`Aggregate Costs`
+simulatedAggregateCostsPlot <- ggplot2::ggplot(data = data.frame(cost = simulatedAggregateCosts), ggplot2::aes(cost)) + 
+    ggplot2::geom_histogram(
+        fill = "gray83",
+        bins = 60
+    ) + 
+    ggplot2::theme_classic() + 
+    ggplot2::labs(
+        y = "Fréquence",
+        x = "Coût"
+    )
+
+##  Get the frequencies to then identify the maximum one for drawing the rectangles sbelow
+countFrequenciesHistogram <- ggplot_build(simulatedAggregateCostsPlot)$data[[1]]$count
+quantiles <- quantile(simulatedAggregateCosts)
+
+##  Plot the data with rectangles for the quantiles and a line of the mean
+simulatedAggregateCostsPlot +
+    geom_rect(
+        aes(
+            xmin = quantiles[1], xmax = quantiles[2], 
+            ymin = 0, ymax = max(countFrequenciesHistogram)
+        ),
+        alpha = 0, fill = "white", colour = "navy", text = "test"
+    )  +
+    geom_rect(
+        aes(
+            xmin = quantiles[2], xmax = quantiles[3],
+            ymin = 0, ymax = max(countFrequenciesHistogram)
+        ),
+        alpha = 0, fill = "white", colour = "navy"
+    ) +
+    geom_rect(
+        aes(
+            xmin = quantiles[3], xmax = quantiles[4],
+            ymin = 0, ymax = max(countFrequenciesHistogram)
+        ),
+        alpha = 0, fill = "white", colour = "navy"
+    ) +
+    geom_rect(
+        aes(
+            xmin = quantiles[4], xmax = quantiles[5],
+            ymin = 0, ymax = max(countFrequenciesHistogram)
+        ),
+        alpha = 0, fill = "white", colour = "navy"
+    ) +
+    ggplot2::geom_vline(
+        ggplot2::aes(xintercept = mean(simulatedAggregateCosts)),
+        linetype = "dashed", colour = "gray3"
+    ) +
+    annotate(
+        geom = "text", 
+        x = mean(simulatedAggregateCosts) + 10, y = 25, 
+        label = 'paste("", bar(paste("Z")))', parse = TRUE # parse le LaTeX
+    )  +
+    geom_text(
+        data = data.frame(perc = (quantiles[-5] + quantiles[-1])/2, med = 300, text = paste0("Q", 1:4)),
+        mapping = aes(x = perc, y = med, label = text)
+    )
